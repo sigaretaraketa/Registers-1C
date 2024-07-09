9d33ae10d3ce9d3e67d490f1a66a3bff054f1bcc
sigaretaraketa <ilya.g.web@yandex.ru>
2024-07-09 12:45:46 +0300

    Add files via upload

--- scripts/copy.js
+++ scripts/copy.js
@@ -1,9 +1,25 @@
-const copyLinks = document.querySelectorAll('.copy-link');
+const copyLinks = document.querySelectorAll('.copy-link'),
+    toast = document.querySelector(".toast");
+(closeIcon = document.querySelector(".close")),
+    (progress = document.querySelector(".progress"));
+let timer1, timer2;
+
 
 copyLinks.forEach((link) => {
     link.addEventListener('click', async (event) => {
+
         event.preventDefault();
         const linkText = link.title;
+        toast.classList.add("active");
+        progress.classList.add("active");
+
+        timer1 = setTimeout(() => {
+            toast.classList.remove("active");
+        }, 1000); //1s = 1000 milliseconds
+
+        timer2 = setTimeout(() => {
+            progress.classList.remove("active");
+        }, 1300);
 
         try {
             const decodedLinkText = decodeURI(linkText);
@@ -13,5 +29,16 @@ copyLinks.forEach((link) => {
         } catch (err) {
             console.error('Failed to copy text: ', err);
         }
+
     });
+});
+closeIcon.addEventListener("click", () => {
+    toast.classList.remove("active");
+
+    setTimeout(() => {
+        progress.classList.remove("active");
+    }, 300);
+
+    clearTimeout(timer1);
+    clearTimeout(timer2);
 });
