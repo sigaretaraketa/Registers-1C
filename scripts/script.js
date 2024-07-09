52475aa78537aff48f54c409a67c2b8c5d9c0f6e
sigaretaraketa <ilya.g.web@yandex.ru>
2024-07-09 08:51:31 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -11,20 +11,20 @@
 //     });
 
 
-const copyLinks = document.querySelectorAll('.copy-link');
+// const copyLinks = document.querySelectorAll('.copy-link');
 
-copyLinks.forEach((link) => {
-    link.addEventListener('click', async (event) => {
-        event.preventDefault();
-        const linkText = link.title;
+// copyLinks.forEach((link) => {
+//     link.addEventListener('click', async (event) => {
+//         event.preventDefault();
+//         const linkText = link.title;
 
-        try {
-            const decodedLinkText = decodeURI(linkText);
+//         try {
+//             const decodedLinkText = decodeURI(linkText);
 
-            await navigator.clipboard.writeText(decodedLinkText);
+//             await navigator.clipboard.writeText(decodedLinkText);
 
-        } catch (err) {
-            console.error('Failed to copy text: ', err);
-        }
-    });
-});
+//         } catch (err) {
+//             console.error('Failed to copy text: ', err);
+//         }
+//     });
+// });
