778f41d385a79af2a86d8b17ee3db5153460b5d7
sigaretaraketa <ilya.g.web@yandex.ru>
2024-07-09 08:52:13 +0300

    Update copy.js

--- scripts/copy.js
+++ scripts/copy.js
@@ -1,1 +1,17 @@
+const copyLinks = document.querySelectorAll('.copy-link');
 
+copyLinks.forEach((link) => {
+    link.addEventListener('click', async (event) => {
+        event.preventDefault();
+        const linkText = link.title;
+
+        try {
+            const decodedLinkText = decodeURI(linkText);
+
+            await navigator.clipboard.writeText(decodedLinkText);
+
+        } catch (err) {
+            console.error('Failed to copy text: ', err);
+        }
+    });
+});
