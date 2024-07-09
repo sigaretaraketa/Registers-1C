fda7d983e73cc53d79cd75a403fdade2ab7b6e35
sigaretaraketa <ilya.g.web@yandex.ru>
2024-07-09 08:45:45 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,30 @@
-document.addEventListener('click',
-    function (event) {
-        let target = event.target.closest('.copyticker');
-        if (target) {
-            event.preventDefault();
-            navigator.clipboard.writeText(target.textContent);
-            window.clearTimeout(target.timer);
-            target.classList.add('copy');
-            target.timer = window.setTimeout(_ => target.classList.remove('copy'), 300)
+// document.addEventListener('click',
+//     function (event) {
+//         let target = event.target.closest('.copyticker');
+//         if (target) {
+//             event.preventDefault();
+//             navigator.clipboard.writeText(target.textContent);
+//             window.clearTimeout(target.timer);
+//             target.classList.add('copy');
+//             target.timer = window.setTimeout(_ => target.classList.remove('copy'), 300)
+//         }
+//     });
+
+
+const copyLinks = document.querySelectorAll('.copy-link');
+
+copyLinks.forEach((link) => {
+    link.addEventListener('click', async (event) => {
+        event.preventDefault();
+        const linkText = link.title;
+
+        try {
+            const decodedLinkText = decodeURI(linkText);
+
+            await navigator.clipboard.writeText(decodedLinkText);
+
+        } catch (err) {
+            console.error('Failed to copy text: ', err);
         }
     });
-
-
+});
